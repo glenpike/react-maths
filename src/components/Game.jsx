42e89b645c1e024f64e500cdcacdfcef63bb42b2
Glen Pike <ski@dijitl.co.uk>
2018-04-02 01:47:27 +0100

    Styling.
    Initial styling with gov.uk guideline colour scheme.
    Deliberately using Comic Sans.
    Create all-in-one style-sheet + component styles.
    TODO: dry up?

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -12,16 +12,12 @@ class Question extends Component {
     } = this.props;
     return (
       <div className="Question">
-        <input className="Question__number" value={firstNum} readOnly />
-        &nbsp;x{' '}
+        <span className="Question__number">{firstNum}</span>
+        &nbsp;x&nbsp;
+        <span className="Question__number">{secondNum}</span>
+        &nbsp;=&nbsp;
         <input
-          className="Question__number--second"
-          value={secondNum}
-          readOnly
-        />
-        &nbsp;={' '}
-        <input
-          className="Question__number--answer"
+          className="Question__number--answer text-input"
           type="number"
           value={givenAnswer}
           onKeyPress={onKeyPress}
@@ -38,13 +34,11 @@ class Score extends Component {
     const time = `${mins} minutes ${secs} seconds`;
     return (
       <div className="Score">
-        <p>
-          <span className="Score__time">{time}</span>
+        <span className="Score__time">{time}</span>
           <br />
           <span className="Score__value">
             {numAnswered} of {numQuestions} answered
           </span>
-        </p>
       </div>
     );
   }
@@ -64,48 +58,58 @@ class Settings extends Component {
   render() {
     const { levels, runTimer } = this.props;
     const levelItems = Object.keys(levels).map((level, idx) => (
-      <li className="Settings__level_item" key={level}>
-        {level}{' '}
+      <li className="Settings__levels-item checkbox primary" key={level}>
         <input
           ref={level}
+          id={level}
           className="checkbox"
           type="checkbox"
           defaultChecked={levels[level]}
         />
+        <label htmlFor={level}>{level}</label>
       </li>
     ));
     return (
       <div className="Settings">
         <form onSubmit={this.onSubmit}>
           <ul className="Settings__levels">{levelItems}</ul>
-          <p>
-            <input
-              className="checkbox"
-              type="checkbox"
-              ref="runTimer"
-              defaultChecked={runTimer}
-            />
-            <input className="button" type="submit" value="Go!" />
-          </p>
+          <ul className="Settings__extra">
+            <li className="Settings__extra-item checkbox primary">
+              <input
+                className="checkbox"
+                type="checkbox"
+                ref="runTimer"
+                id="runTimer"
+                defaultChecked={runTimer}
+              />
+              <label htmlFor="runTimer">Timer</label>
+            </li>
+            <li className="Settings__extra-item checkbox primary">
+              <input className="button" type="submit" value="Go!" />
+            </li>
+          </ul>
         </form>
       </div>
     );
   }
 }
 
-class PreviousAnswers extends Component {
+class Answers extends Component {
   render() {
     const { answers } = this.props;
     const answerItems = answers.map((answer, idx) => (
-      <li className="Answers__list-item" key={idx}>
+      <li
+        className={
+          'Answers__list-item ' +
+          (answer.givenAnswer === answer.actual ? 'correct' : 'wrong')
+        }
+        key={idx}
+      >
         {answer.firstNum} x {answer.secondNum} = {answer.givenAnswer}
-        <span
-          className={
-            'Answers__answer--' +
-            (answer.givenAnswer === answer.actual ? 'correct' : 'wrong')
-          }
-        >
-          {answer.givenAnswer === answer.actual ? '' : answer.actual}
+        <span className="Answers__answer">
+          {answer.givenAnswer === answer.actual
+            ? ''
+            : 'Correct answer: ' + answer.actual}
         </span>
       </li>
     ));
@@ -208,7 +212,7 @@ class Game extends Component {
     const { timer } = this.state;
     clearInterval(timer.interval);
     timer.interval = null;
-    // show "finished!"
+    // show 'finished!'
     this.setState({
       playing: false,
       timer,
@@ -293,7 +297,7 @@ class Game extends Component {
           numAnswered={answers.length}
           numQuestions={numQuestions}
         />
-        <PreviousAnswers answers={answers} />
+        <Answers answers={answers} />
       </div>
     );
   }
